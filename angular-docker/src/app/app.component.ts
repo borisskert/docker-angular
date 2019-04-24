cc0c17ffb681a4318884d376c0fb99c8b6141b66
Boris Skert <boris.skert@adorsys.de>
2019-04-24 08:54:56 +0200

    Reformat code

--- angular-docker/src/app/app.component.ts
+++ angular-docker/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import {AppConfig} from "./env/app-config";
+import { AppConfig } from './env/app-config';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
   constructor(private appConfig: AppConfig) {
